a5ab3b5cb72ef12dacfc60248a65c4fbe57fe0fd
caobaichuan1122 <caobaichuan1122@gmail.com>
2022-08-05 01:08:17 +1000

    version1

---
+++ login/models.py
@@ -0,0 +1,26 @@
+# login/models.py
+
+from django.db import models
+
+
+class User(models.Model):
+
+    gender = (
+        ('male'),
+        ('female'),
+    )
+
+    #create the user info
+    name = models.CharField(max_length=128,unique=True)
+    password = models.CharField(max_length=256)
+    email = models.EmailField(unique=True)
+    sex = models.CharField(max_length=32,choices=gender,default='male')
+    c_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['c_time']
+        verbose_name = 'user'
+        verbose_name_plural = 'user'
