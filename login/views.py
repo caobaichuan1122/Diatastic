80afbeb81a8939dcf59ccebd9c706bda0b491492
caobaichuan1122 <272726431@163.com>
2022-08-05 15:22:47 +1000

    version3

--- login/views.py
+++ login/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'login.html', locals())
+    return render(request, 'home/index.html')
