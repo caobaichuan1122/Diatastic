2aa7e7b90c84177a9abdc6ae2e79a6fc11bacf6b
Luck Wei <luckwei123@gmail.com>
2022-08-08 00:54:34 +1000

    merged functionality from jsontopandas function into the api call + further optimisations

--- database/api.py
+++ database/api.py
@@ -1,13 +1,11 @@
-import time
-
+from datetime import datetime as dt
+from time import sleep
 import pandas as pd
 import requests
 
-from JSONtoPD import JSONtoPandas
-
 # Load postcodes from csv
 postcodes_csv_path = "./database/Postcodes.csv"
-postcodes_coords_df = pd.read_csv(postcodes_csv_path).head(5)
+postcodes_coords_df = pd.read_csv(postcodes_csv_path)
 
 API_KEY = "3b8bfa253dd09a9f809435251aded64b"
 
@@ -16,10 +14,10 @@ exclusion_list = ["current", "minutely", "daily", "alerts"]
 exclusions = ",".join(exclusion_list)
 
 
-def api_call(delay=2):
+def api_call(delay=2) -> pd.DataFrame:
 
-    # Empty DF to be concatenated with new rows
-    final_df = pd.DataFrame()
+    # Empty list to be added with new rows of data
+    final_data_list = []
 
     # Iterate through postcodes
     for postcode, lat, long in postcodes_coords_df.itertuples(index=False):
@@ -30,20 +28,26 @@ def api_call(delay=2):
 
         # If successful, read the response
         if response.status_code == 200:
-            print(f"Success!\t{postcode=}\t{lat=}\t{long=}")
+            print(f"SUCCESS! \t{postcode=}\t{lat=:>9.4f}\t{long=:>9.4f}")
+            
+            weather_df = pd.json_normalize(response.json(), record_path=["hourly"])
 
-            # Convert response into postcode
-            API_df = JSONtoPandas(response.json())
+            postcode_weather_data = [
+                {
+                    "postcode": postcode,
+                    "timestamp": dt.fromtimestamp(row.dt),
+                    "temp": round(row.temp - 273.15, 2),
+                    "uv": row.uvi,
+                }
+                for row in weather_df.itertuples(index=False)
+            ]
 
-            # Include postcode data
-            API_df[["postcode", "lat", "lng"]] = postcode, lat, long
+            # Append to list
+            final_data_list += postcode_weather_data
 
-            # Concatenate new rows to final df
-            final_df = pd.concat([final_df, API_df], ignore_index=True)
+            sleep(delay)
 
-        # Delay to reduce the load on the API
-        time.sleep(delay)
-
+    final_df = pd.DataFrame(final_data_list)
     return final_df
 
 
