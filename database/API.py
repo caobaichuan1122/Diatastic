ab7f74b434762050da3fe8501a89635fbf49fe2d
Luck Wei <luckwei123@gmail.com>
2022-08-07 23:26:22 +1000

    bring exclusions declaration & C:/Users/LuckWei/AppData/Local/Programs/Python/Python310/python.exe c:/Users/LuckWei/Desktop/TP08/database/API.pyout of api call

--- database/API.py
+++ database/API.py
@@ -1,29 +1,26 @@
-# Essentials.
 import time
-
 import pandas as pd
-
-# API request function.
 import requests
 
 from JSONtoPD import JSONtoPandas
 
-postcodes_coords_df = pd.read_csv("./database/Postcodes.csv").head(5)
+# Load postcodes from csv
+postcodes_csv_path = "./database/Postcodes.csv"
+postcodes_coords_df = pd.read_csv(postcodes_csv_path).head(5)
+
 API_KEY = "3b8bfa253dd09a9f809435251aded64b"
 
-# For each postcode in the dataset, call the API.
-def api_call(delay):
+# Exclude all metrics other than `hourly`
+exclusion_list = ["current", "minutely", "daily", "alerts"]
+exclusions = ",".join(exclusion_list)
+
+def api_call(delay=4):
 
     # Empty DF to be concatenated with new rows
     final_df = pd.DataFrame()
 
-    # Iterate through all postcodes
+    # Iterate through postcodes
     for postcode, lat, long in postcodes_coords_df.itertuples(index=False):
-        print(f"Retrieving data from the API\t{postcode=}\t{lat=}\t{long=}")
-
-        # Exclude all metrics other than `hourly`
-        exclusion_list = ["current", "minutely", "daily", "alerts"]
-        exclusions = ",".join(exclusion_list)
 
         # Call the API
         url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={long}&exclude={exclusions}&appid={API_KEY}"
@@ -31,15 +28,13 @@ def api_call(delay):
 
         # If successful, read the response
         if response.status_code == 200:
-            print("Successful!")
+            print(f"Success!\t{postcode=}\t{lat=}\t{long=}")
 
             # Convert response into postcode
             API_df = JSONtoPandas(response.json())
 
             # Include postcode data
-            API_df["postcode"] = postcode
-            API_df["lat"] = lat
-            API_df["lng"] = long
+            API_df[["postcode", "lat", "lng"]] = postcode, lat, long
 
             # Concatenate new rows to final df
             final_df = pd.concat([final_df, API_df], ignore_index=True)
