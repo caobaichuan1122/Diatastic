1dfeeb5bbdcb14b0a69bfd09a19df32e2f2a15d4
Rashwinder <110654543+Rashwinder@users.noreply.github.com>
2022-08-07 16:50:12 +1000

    Version 1
    No connection to db.

--- database/main.py
+++ database/main.py
@@ -1,3 +1,4 @@
+# Scripts.
 # Sends a call to the API.
 from API import *
 # Updates the database.
@@ -5,11 +6,14 @@ from Update import *
 
 # This loop will keep running and activate every 24 hours.
 # This is caused by the internal restriction in the DBaseUpdate function
-# while True:
-# Full dataset with 48 entries per postcode, for 722 postcodes.
-Hourly = API_Call()
-print(Hourly.head())
+while True:
+    # Full dataset with 48 entries per postcode, for 722 postcodes.
+    Hourly = API_Call()
+    print(Hourly.head())
 
-# Execute update.
-print('\nChecking timer since last update.\nPlease stand by..')
-DbaseUpdate(Hourly)
+    # Execute update.
+    print('\nChecking timer since last update.\nPlease stand by..')
+    DbaseUpdate(Hourly)
+
+    # Update the last active date in the tracker text file.
+    tracker()
