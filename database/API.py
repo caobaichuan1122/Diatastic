bb64559acbbc3173fab33e74ba9fd4e598c1bbaf
Luck Wei <luckwei123@gmail.com>
2022-08-07 23:29:45 +1000

    reduced default delay of API call from 4 to 2 seconds

--- database/API.py
+++ database/API.py
@@ -1,4 +1,5 @@
 import time
+
 import pandas as pd
 import requests
 
@@ -14,7 +15,8 @@ API_KEY = "3b8bfa253dd09a9f809435251aded64b"
 exclusion_list = ["current", "minutely", "daily", "alerts"]
 exclusions = ",".join(exclusion_list)
 
-def api_call(delay=4):
+
+def api_call(delay=2):
 
     # Empty DF to be concatenated with new rows
     final_df = pd.DataFrame()
@@ -24,7 +26,7 @@ def api_call(delay=4):
 
         # Call the API
         url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={long}&exclude={exclusions}&appid={API_KEY}"
-        response = requests.get(url=url)
+        response = requests.get(url)
 
         # If successful, read the response
         if response.status_code == 200:
