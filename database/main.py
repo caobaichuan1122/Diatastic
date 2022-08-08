e008c8f0f94c8e38c0751fb200f671b252e77a5d
Luck Wei <luckwei123@gmail.com>
2022-08-08 23:38:33 +1000

    set code in main to safely run, unforced

--- database/main.py
+++ database/main.py
@@ -4,7 +4,7 @@ config_path = "./database/.ini"
 postcodes_csv_path = "./database/Postcodes.csv"
 API_KEY = (
     "3a178218d81590de8682b7e41b33743e" or "3b8bfa253dd09a9f809435251aded64b"
-)  # Luck's or Rash's
+)  # Luck's or Rash's, short-circuits to first api key 
 
 host = "3.25.191.104"
 user = "mysql"
@@ -24,7 +24,7 @@ def main() -> None:
         database=database,
         port=port,
     )
-    app.perpetual_run_daily(table="weather_2", forced=True)
+    app.perpetual_run_daily(table="weather", forced=False)
 
 
 if __name__ == "__main__":
