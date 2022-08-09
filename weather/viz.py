71a50c33b9b66421f354671c447d0e9ed4cb834b
Luck Wei <luckwei123@gmail.com>
2022-08-09 15:34:23 +1000

    clean up code lines that were commented out

--- weather/viz.py
+++ weather/viz.py
@@ -42,38 +42,6 @@ def construct_viz(user, password, host, port, database, postcode):
     # TODO:Push "./weather/uv_viz.png" to html in website
 
 
-# class Visualization:
-#     def __init__(self, user, password, host, port, database):
-#         # Set up connection to MySql database
-#         self.engine = create_engine(
-#             f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
-#         )
-
-#     def show_viz(self, postcode):
-#         # Pull data as df from db
-#         df = pd.read_sql("weather", self.engine)
-
-#         # Filter out postcode and from future datetimes
-#         df = df.loc[
-#             (df["postcode"] == postcode)
-#             & (df["timestamp"] > dt.now() - timedelta(hours=1))
-#             & (df["timestamp"] < dt.now() + timedelta(hours=24))
-#         ]
-
-#         def save_viz(col):
-#             plt.plot(df["timestamp"].apply(lambda x: x.strftime("%H")), df[col], linestyle="--", marker="o", color="b")
-#             plt.xlabel("timestamp")
-#             plt.ylabel(col)
-#             plt.savefig(f"./weather/demo-{col}.png")
-#             plt.close()
-
-
-#         save_viz("temp")
-#         save_viz("uv")
-
-#         # TODO:Push these png files to the website
-
-
 def main() -> None:
     host = "3.25.191.104"
     user = "mysql"
@@ -83,7 +51,14 @@ def main() -> None:
 
     postcode = 3996  # Change this
 
-    construct_viz(host=host, user=user, password=password, database=database, port=port, postcode=postcode)
+    construct_viz(
+        host=host,
+        user=user,
+        password=password,
+        database=database,
+        port=port,
+        postcode=postcode,
+    )
 
 
 if __name__ == "__main__":
