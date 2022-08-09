ad71163232bc10ddb50ea4e7c2e273603f344dea
Luck Wei <luckwei123@gmail.com>
2022-08-09 15:31:46 +1000

    replaced Vizualisation class to a single function

--- weather/viz.py
+++ weather/viz.py
@@ -3,60 +3,75 @@ from datetime import timedelta
 
 import matplotlib.pyplot as plt
 import pandas as pd
-# import plotly.express as px
-# import plotly.graph_objects as go
 from sqlalchemy import create_engine
 
 
-class Visualization:
-    def __init__(self, user, password, host, port, database):
-        # Set up connection to MySql database
-        self.engine = create_engine(
-            f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+def construct_viz(user, password, host, port, database, postcode):
+    # Set up connection to MySql database
+    engine = create_engine(
+        f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+    )
+
+    # Pull data as df from db
+    df = pd.read_sql("weather", engine)
+
+    # Filter out postcode and from future datetimes
+    df = df.loc[
+        (df["postcode"] == postcode)
+        & (df["timestamp"] > dt.now() - timedelta(hours=1))
+        & (df["timestamp"] < dt.now() + timedelta(hours=24))
+    ]
+
+    def save_fig(col, file_name):
+        plt.plot(
+            df["timestamp"].apply(lambda x: x.strftime("%H")),
+            df[col],
+            linestyle="--",
+            marker="o",
+            color="b",
         )
+        plt.xlabel("timestamp")
+        plt.ylabel(col)
+        plt.savefig(file_name)
+        plt.close()
 
-    def show_viz(self, postcode):
-        # Pull data as df from db
-        df = pd.read_sql("weather", self.engine)
+    save_fig("temp", "./weather/temp_viz.png")
+    save_fig("uv", "./weather/uv_viz.png")
 
-        # Filter out postcode and from future datetimes
-        df = df.loc[
-            (df["postcode"] == postcode)
-            & (df["timestamp"] > dt.now() - timedelta(hours=1))
-            & (df["timestamp"] < dt.now() + timedelta(hours=24))
-        ]
-        
-        def save_viz(col):
-            plt.plot(df["timestamp"].apply(lambda x: x.strftime("%H")), df[col], linestyle="--", marker="o", color="b")
-            plt.xlabel("timestamp")
-            plt.ylabel(col)
-            plt.savefig(f"./weather/demo-{col}.png")
-            plt.close()
-            
-        
-        save_viz("temp")
-        save_viz("uv")
-
-        # TODO:Push these png files to the website
+    # TODO:Push "./weather/temp_viz.png" to html in website
+    # TODO:Push "./weather/uv_viz.png" to html in website
 
 
-# # Data
-# hourly = pd.read_csv('C:/Users/HP-user/OneDrive/Desktop/Test2.csv')
+# class Visualization:
+#     def __init__(self, user, password, host, port, database):
+#         # Set up connection to MySql database
+#         self.engine = create_engine(
+#             f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+#         )
 
-# # Temperature.
-# fig2 = px.line(hourly, x = 'timestamp', y = 'temp')
-# fig2.add_trace(go.Scatter(x = hourly['timestamp'], y = hourly['temp'],
-#                           mode = 'lines+markers')
-#                )
-# fig2.show()
+#     def show_viz(self, postcode):
+#         # Pull data as df from db
+#         df = pd.read_sql("weather", self.engine)
 
-# # Ultraviolet.
-# fig2 = px.line(hourly, x = 'timestamp', y = 'uvi')
-# fig2.add_trace(go.Scatter(x = hourly['timestamp'], y = hourly['uvi'],
-#                           line = dict(color = 'royalblue'),
-#                           mode = 'lines+markers')
-#               )
-# fig2.show()
+#         # Filter out postcode and from future datetimes
+#         df = df.loc[
+#             (df["postcode"] == postcode)
+#             & (df["timestamp"] > dt.now() - timedelta(hours=1))
+#             & (df["timestamp"] < dt.now() + timedelta(hours=24))
+#         ]
+
+#         def save_viz(col):
+#             plt.plot(df["timestamp"].apply(lambda x: x.strftime("%H")), df[col], linestyle="--", marker="o", color="b")
+#             plt.xlabel("timestamp")
+#             plt.ylabel(col)
+#             plt.savefig(f"./weather/demo-{col}.png")
+#             plt.close()
+
+
+#         save_viz("temp")
+#         save_viz("uv")
+
+#         # TODO:Push these png files to the website
 
 
 def main() -> None:
@@ -65,14 +80,10 @@ def main() -> None:
     password = "TP08"
     database = "tp08_website"
     port = 3306
-    
+
     postcode = 3996  # Change this
-    
-    viz = Visualization(
-        user=user, host=host, password=password, database=database, port=port
-    )
-    
-    viz.show_viz(postcode)
+
+    construct_viz(host=host, user=user, password=password, database=database, port=port, postcode=postcode)
 
 
 if __name__ == "__main__":
