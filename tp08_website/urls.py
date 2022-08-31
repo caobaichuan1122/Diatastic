ab0ed2d02c2a8d8e97317d510328a457268ec190
caobaichuan1122 <272726431@163.com>
2022-08-31 15:30:56 +1000

    version1

--- tp08_website/urls.py
+++ tp08_website/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('diary/', views.diary, name='diary'),
-    path('^favicon\.ico$', RedirectView.as_view(url='/static/media/favicon.ico')),
+    path('favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
