b5697e8303e4040947a5fbbdbc528b2df3527aa2
Luck Wei Cheong <luckwei123@gmail.com>
2022-09-20 15:50:11 +1000

    added links to helpbot responses

--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -33,22 +33,22 @@ const keywords = {
 
 const responses = {
     0: "Diabetes occurs when your blood glucose from the food you eat becomes too high. Insulin helps move glucose from our bloodstream into the body's cells to make energy but for some individuals, the pancreas does not make enough or use insulin well.",
-    1: "There are 3 main types, type 1, type 2 and gestational diabetes, all being complex and serious.",
-    2: "Type 1 diabetes is a chronic medical condition where cells that make insulin are destroyed, hence the body is unable to process glucose due to the lack of insulin.",
-    3: "Type 2 diabetes is a chronic medical condition where your body is not able to respond to insulin as well as it should. In later stages, it also many not produce enough insulin. When uncontrolled, may lead to chronically high blood glucose levels, leading to serious complications.",
-    4: "Gestational diabetes typically develops between the 24th to 28th week of pregnancy. Although not indicative of having diabetes before or after pregnancy, it raises the risk of type 2 diabetes in the future. If poorly managed, it can raise the child's chance of developing diabetes and create complications during pregnancy and delivery.",
-    5: "Hypoglycemia happens when blood glucose falls low, possibly a side effect of insulin. Severely low blood glucose can cause serious complications, including passing out, coma, or death. Symptoms include rapid heartbeat, sweating, confusion, slurred speech, numbness in fingers, toes and lips",
-    6: "Being thirsty, needing to pee, always feeling hungry, itching, losing weight",
-    7: "Available commands: ?diet, ?treatment",
-    8: "//Healthy eating is essential etc. REDIRECTING YOU TO THE GUIDE//",
-    9: "//Explore your treatment options here, includes all types of diabetes. REDIRECTING YOU TO THE GUIDE//",
-    10: "HHNS is when your blood glucose level goes way too high, and if you don’t treat it, it can cause death.",
-    11: "Likely to develop heart disease, nerve damage (diabetic neuropathy), kidney damage, diabetic eye disease",
-    12: "Type 1: Insulin, Type 2: Diabetic pills and/or non-insulin injectables",
-    13: "Yes: Fruits, veges, legummes, poultry, avocados, nuts, seeds. No: Sugar, candy, soda, hot dogs, fatty red meat, butter",
-    14: "Check blood sugar 3-4 times a day, avoid skipping meals, exercise more, get enough sleep",
-    15: "Test blood glucose levels regularly, take insulin, stay active, have a healthy eating plan",
-    16: "//REDIRECTING YOU TO THE DIARY PAGE//"
+    1: "There are 3 main types, type 1, type 2 and gestational diabetes, all being complex and serious. <a href='../guide#types'>Read more</a>",
+    2: "Type 1 diabetes is a chronic medical condition where cells that make insulin are destroyed, hence the body is unable to process glucose due to the lack of insulin. <a href='../guide#type1'>Read more</a>",
+    3: "Type 2 diabetes is a chronic medical condition where your body is not able to respond to insulin as well as it should. In later stages, it also many not produce enough insulin. When uncontrolled, may lead to chronically high blood glucose levels, leading to serious complications.  <a href='../guide#type2'>Read more</a>",
+    4: "Gestational diabetes typically develops between the 24th to 28th week of pregnancy. Although not indicative of having diabetes before or after pregnancy, it raises the risk of type 2 diabetes in the future. If poorly managed, it can raise the child's chance of developing diabetes and create complications during pregnancy and delivery.  <a href='../guide#gestational'>Read more</a>",
+    5: "Hypoglycemia happens when blood glucose falls low, possibly a side effect of insulin. Severely low blood glucose can cause serious complications, including passing out, coma, or death. Symptoms include rapid heartbeat, sweating, confusion, slurred speech, numbness in fingers, toes and lips  <a href='../guide#hypoglycemia'>Read more</a>",
+    6: "Being thirsty, needing to pee, always feeling hungry, itching, losing weight  <a href='../guide#symptoms'>Read more</a>",
+    7: "Available commands:<br>?diet, ?treatment, ?diary",
+    8: "Healthy eating is essential! REDIRECTING YOU TO THE GUIDE",
+    9: "Explore your treatment options! REDIRECTING YOU TO THE GUIDE",
+    10: "HHNS is when your blood glucose level goes way too high, and if you don't treat it, it can cause death.  <a href='../guide#hhns'>Read more</a>",
+    11: "Likely to develop heart disease, nerve damage (diabetic neuropathy), kidney damage, diabetic eye disease  <a href='../guide#longterm'>Read more</a>",
+    12: "Type 1: Insulin, Type 2: Diabetic pills and/or non-insulin injectables  <a href='../guide#treatment'>Read more</a>",
+    13: "Yes: Fruits, veges, legummes, poultry, avocados, nuts, seeds. No: Sugar, candy, soda, hot dogs, fatty red meat, butter  <a href='../guide#diet'>Read more</a>",
+    14: "Check blood sugar 3-4 times a day, avoid skipping meals, exercise more, get enough sleep  <a href='../guide#routine'>Read more</a>",
+    15: "Test blood glucose levels regularly, take insulin, stay active, have a healthy eating plan  <a href='../guide#habits'>Read more</a>",
+    16: "Keeping track of your diet is key to diabeties management! REDICTING YOU TO THE DIARY"
 };
 
 const stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
@@ -80,10 +80,10 @@ function respond(user_input) {
     if (user_input in keywords) {
         switch (keywords[user_input]) {
             case 8:
-                window.location.href = "../guide"
+                window.location.href = "../guide#diet"
                 break;
             case 9:
-                window.location.href = "../guide"
+                window.location.href = "../guide#treatment"
                 break;
             case 16:
                 window.location.href = "../diary"
